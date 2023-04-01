274166f946182a81d21032e98983cf6dcbdb3078
Basilica Anthony <94911634+basilicaanthony@users.noreply.github.com>
2023-04-01 12:26:17 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express()
+const port =3000
+app.listen(port,function(){
+  console.log("hello world")
+});
+app.get("/",function(req,res){
+  res.send("this is running by server 3000")
+});
